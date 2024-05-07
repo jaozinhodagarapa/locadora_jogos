1a41fe625dc9eec3d31555ad3cc9b9aa029e31e8
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2024-05-07 15:21:48 -0300

    Atualizado

--- app/Http/Requests/JogosFormRequest.php
+++ app/Http/Requests/JogosFormRequest.php
@@ -24,43 +24,52 @@ class JogosFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nome'=>'required|max:120|min:5|unique:jogos,nome',
-            'preco'=>'required|decimal:12.2',
-            'descricao'=>'required|max:800|min:10',
-            'classificacao'=>'required|max:20|min:5|',
-            'plataformas'=>'required|max:60|min:3',
-            'desenvolvedor'=>'required|max:120|min:2',
-            'distribuidora'=>'required|max:120|min:2', 
-            'categoria'=>'required|max:55|min:3'
+            'nome' => 'required|max:120|min:5|unique:jogos,nome',
+            'preco' => 'required|decimal:12,2',
+            'descricao' => 'required|max:800|min:10',
+            'classificacao' => 'required|max:20|min:5',
+            'plataformas' => 'required|max:60|min:3',
+            'desenvolvedor' => 'required|max:120|min:2',
+            'distribuidora' => 'required|max:120|min:2',
+            'categoria' => 'required|max:55|min:3'
         ];
     }
-    public function failedValidation(Validator $validator){
+
+    public function failedValidation(Validator $validator)
+    {
         throw new HttpResponseException(response()->json([
-            'status' => false,
+            'success' => false,
             'error' => $validator->errors()
         ]));
-}
+    }
 
-public function messages(){
-    return [
-        'nome.required' => 'Nome obrigatório',
-        'nome.max' => 'Nome deve conter no máximo 120 caracteres',
-        'nome.min' => 'Nome deve conter no mínimo 5 caracteres',
-        'nome.unique' => 'Nome já cadastrado no sistema',
-        'celular.required' => 'Celular obrigatório',
-        'celular.max' => 'Celular deve conter no máximo 11 caracteres',
-        'email.required' => 'E-mail obrigatório',
-        'email.max' => 'E-mail deve conter no máximo 120 caracteres',
-        'email.email' => 'Formato de e-mail inválido',
-        'email.unique' => 'E-mail já cadastrado no sistema',
-        'cpf.required' => 'CPF obrigatório',
-        'cpf.max' => 'CPF deve conter no máximo 11 caracteres',
-        'cpf.min' => 'CPF deve conter no mínimo 11 caracteres',
-        'cpf.unique' => 'CPF já cadastrado no sistema',
-        'dataNascimento.required' => 'Data de nascimento obrigatório',
-        'dataNascimento.date' => 'O campo deve conter apenas datas',
-        'cidade.required' => 'Cidade obrigatório',
-        'cidade.max' => 'Cidade deve conter no máximo 120 caracteres',
-    ];
-}
+    public function messages()
+    {
+        return [
+            'nome.required' => 'O campo Nome é obrigatório',
+            'nome.max' => 'O campo Nome deve conter no máximo 120 caracteres',
+            'nome.min' => 'O campo Nome deve conter no mínimo 5 caracteres',
+            'nome.unique' => 'Nome já cadastrado no sistema',
+            'preco.required' => 'O campo Celular é obrigatório',
+            'preco.decimal' => 'O campo Preço deve conter apenas valores em decimais',
+            'descricao.required' => 'O campo Descrição é obrigatório',
+            'descricao.max' => 'O campo Descrição deve conter no máximo 800 caracteres',
+            'descricao.min' => 'O campo Descriçaõ deve conter no mínimo 10 caracteres',
+            'classificacao.required' => 'O campo Classificacao é obrigatório',
+            'classificacao.max' => 'O campo Classificacao deve conter no máximo 20 caracteres',
+            'classificacao.min' => 'O campo Classificacao deve conter no mínimo 5 caracteres',
+            'plataformas.required' => 'O campo Plataforma é obrigatório',
+            'plataformas.max' => 'O campo Plataforma deve conter no máximo 60 caracteres',
+            'plataformas.min' => 'O campo Plataforma deve conter no mínimo 3 caracteres',
+            'desenvolvedor.required' => 'O campo Desenvolvedor é obrigatório',
+            'desenvolvedor.max' => 'O campo Desenvolvedor deve conter no máximo 120 caracteres',
+            'desenvolvedor.min' => 'O campo Desenvolvedor deve conter no mínimo 2 caracteres',
+            'distribuidora.required' => 'O campo Distribuidora é obrigatório',
+            'distribuidora.max' => 'O campo Distribuidora deve conter no máximo 120 caracteres',
+            'distribuidora.min' => 'O campo Distribuidora deve conter no mínimo 2 caracteres',
+            'categoria.required' => 'O campo Categoria é orbigatório',
+            'categoria.max' => 'O campo Categoria deve conter no máximo 55 caracteres',
+            'categoria.min' => 'O campo Categoria deve conter no mínimo 3 caracteres'
+        ];
+    }
 }
--- database/migrations/2024_05_07_170429_create_jogos_table.php
+++ database/migrations/2024_05_07_170429_create_jogos_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('jogos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 120)->unique()->nullable(false);
+            $table->decimal('preco', 12, 2)->nullable(false);
+            $table->string('descricao', 800)->nullable(false);
+            $table->string('classificacao', 20)->nullable(false);
+            $table->string('plataformas', 60)->nullable(false);
+            $table->string('desenvolvedor', 120)->nullable(false);
+            $table->string('distribuidora', 120)->nullable(false);
+            $table->string('categoria', 55)->nullable(false);
             $table->timestamps();
         });
     }
